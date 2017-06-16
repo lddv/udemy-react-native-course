af56808557a7d87ed439f4e90f9c0acb7f088bb3
lddv <leodias.vasconcelos@yahoo.com.br>
2017-06-16 20:18:17 -0300

    Redux Thunk async, bending rules.

---
+++ manager/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBHaD4S9tfE60D6Fb5ekgVxS1XutBB9yhU',
+      authDomain: 'manager-80fd4.firebaseapp.com',
+      databaseURL: 'https://manager-80fd4.firebaseio.com',
+      projectId: 'manager-80fd4',
+      storageBucket: 'manager-80fd4.appspot.com',
+      messagingSenderId: '371989994658'
+    };
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ manager/src/components/LoginForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged, loginUser, passwordChanged } from '../actions';
+import { Button, Card, CardSection, Input } from './common';
+
+class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+            value={this.props.password}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button
+            onPress={this.onButtonPress.bind(this)}
+          >
+            Login
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  email: state.auth.email,
+  password: state.auth.password
+});
+
+export default connect(mapStateToProps, {
+  emailChanged, loginUser, passwordChanged
+})(LoginForm);
