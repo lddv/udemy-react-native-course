8e735ffe515f345629b9572a325a1435355c810c
Leonardo Dias de Vasconcelos <leonardo.vasconcelos@hotmart.com.br>
2017-07-04 18:11:12 -0300

    End of section 18.

--- manager/src/reducers/EmployeeFormReducer.js
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,5 @@
 import {
+  EMPLOYEE_CREATE,
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
@@ -12,6 +13,8 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMPLOYEE_UPDATE:
       return { ...state, [action.payload.prop]: action.payload.value };
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
     default:
       return state;
   }
